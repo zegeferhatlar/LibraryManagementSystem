9d575dd029968a695a8c1ead116f4224a3b30b50
Zeynep Gülbahar Ferhatlar <zegeferhatlar@Zeynep-MacBooks-Air.local>
2026-01-03 21:33:30 +0300

    docs: main ve interface için Javadoc eklendi

--- src/main/java/com/zegeferhatlar/library/service/Searchable.java
+++ src/main/java/com/zegeferhatlar/library/service/Searchable.java
@@ -3,9 +3,23 @@ package com.zegeferhatlar.library.service;
 import com.zegeferhatlar.library.model.Book;
 import java.util.List;
 
+/**
+ * Arama yeteneği sağlayan arayüz.
+ */
 public interface Searchable {
 
+    /**
+     * Başlığa göre kitap arar.
+     * @param title aranan başlık (tam/partial)
+     * @return bulunan kitap listesi
+     */
     List<Book> searchByTitle(String title);
 
+    /**
+     * Yazara göre kitap arar.
+     * @param author aranan yazar
+     * @return bulunan kitap listesi
+     */
     List<Book> searchByAuthor(String author);
 }
+
